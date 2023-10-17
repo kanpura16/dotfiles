cb2d48fbbc7ec60a6efe5170874303b7e1b11014
haruwapon <haruwapon@gmail.com>
2023-10-18 04:43:53 +0900

    update

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,60 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+local config = {
+  color_scheme = 'tokyonight',
+  font = wezterm.font("JetBrainsMono NF"),
+  use_ime = true,
+  -- window_background_opacity = 0.6,
+  window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+  hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = false,
+  disable_default_key_bindings = true,
+}
+
+config.keys = {
+  { key = 'j', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(-1) },
+  { key = 'k', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(1) },
+  { key = 'j', mods = 'CTRL|SHIFT|ALT', action = act.MoveTabRelative(-1) },
+  { key = 'k', mods = 'CTRL|SHIFT|ALT', action = act.MoveTabRelative(1) },
+  { key = 't', mods = 'CTRL|SHIFT', action = act.SpawnTab "CurrentPaneDomain" },
+  { key = 'q', mods = 'CTRL|SHIFT', action = act.CloseCurrentTab { confirm = false } },
+
+  { key = 'h', mods = 'ALT|SUPER', action = act.ActivatePaneDirection 'Left' },
+  { key = 'j', mods = 'ALT|SUPER', action = act.ActivatePaneDirection 'Down' },
+  { key = 'k', mods = 'ALT|SUPER', action = act.ActivatePaneDirection 'Up' },
+  { key = 'l', mods = 'ALT|SUPER', action = act.ActivatePaneDirection 'Right' },
+  { key = 'h', mods = 'ALT|SUPER|SHIFT', action = act.RotatePanes 'CounterClockwise' },
+  { key = 'l', mods = 'ALT|SUPER|SHIFT', action = act.RotatePanes 'Clockwise' },
+  { key = 'h', mods = 'ALT|SUPER|CTRL', action = act.AdjustPaneSize { 'Left', 6 } },
+  { key = 'j', mods = 'ALT|SUPER|CTRL', action = act.AdjustPaneSize { 'Down', 6 } },
+  { key = 'k', mods = 'ALT|SUPER|CTRL', action = act.AdjustPaneSize { 'Up', 6 } },
+  { key = 'l', mods = 'ALT|SUPER|CTRL', action = act.AdjustPaneSize { 'Right', 6 } },
+  { key = 'v', mods = 'ALT|SUPER', action = act.SplitHorizontal },
+  { key = 's', mods = 'ALT|SUPER', action = act.SplitVertical },
+  { key = 'q', mods = 'ALT|SUPER', action = act.CloseCurrentPane { confirm = false } },
+
+  { key = 'u', mods = 'CTRL|SHIFT', action = act.ScrollByPage(-0.6) },
+  { key = 'd', mods = 'CTRL|SHIFT', action = act.ScrollByPage(0.6) },
+  { key = 'p', mods = 'CTRL|SHIFT', action = act.ScrollToPrompt(-1) },
+  { key = 'n', mods = 'CTRL|SHIFT', action = act.ScrollToPrompt(1) },
+
+  { key = 'c', mods = 'CTRL|SHIFT', action = act.CopyTo 'Clipboard' },
+  { key = 'v', mods = 'CTRL|SHIFT', action = act.PasteFrom 'Clipboard' },
+  { key = '-', mods = 'CTRL', action = act.DecreaseFontSize },
+  { key = '=', mods = 'CTRL', action = act.IncreaseFontSize },
+  { key = '0', mods = 'CTRL', action = act.ResetFontSize },
+
+  { key = 's', mods = 'CTRL|SHIFT', action = act.QuickSelect },
+  { key = 'f', mods = 'CTRL|SHIFT', action = act.Search { CaseSensitiveString = "" } },
+  { key = 'x', mods = 'CTRL|SHIFT', action = act.ActivateCopyMode },
+}
+
+for i = 1, 9 do
+  table.insert(
+    config.keys,
+    { key = tostring(i), mods = 'CTRL|SHIFT', action = act.ActivateTab(i - 1) }
+  )
+end
+
+return config
---
+++ zsh/.zshrc
@@ -0,0 +1,93 @@
+eval "$(sheldon source)"
+
+autoload -Uz compinit && compinit
+zstyle ':completion:*' completer _complete _approximate _correct _prefix
+zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*:default' list-colors di=34
+
+setopt correct
+setopt ignore_eof
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushd_silent
+setopt share_history
+setopt hist_ignore_all_dups
+setopt inc_append_history
+
+fzf-select-history() {
+    BUFFER=$(history -n -r 1 | fzf --query "$LBUFFER")
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+zle -N fzf-select-history
+bindkey '^r' fzf-select-history
+
+fzf-z-search() {
+    local res=$(z | sort -rn | cut -c 12- | fzf)
+    if [ -n "$res" ]; then
+        BUFFER+="cd $res"
+        zle accept-line
+    fi
+}
+zle -N fzf-z-search
+bindkey '^z' fzf-z-search
+
+fd() {
+    local dir
+    dir=$(find ${1:-.} -path '*/\.*' -prune \
+                    -o -type d -print 2> /dev/null | fzf +m) &&
+    cd "$dir"
+    zle reset-prompt
+}
+zle -N fd
+bindkey '^g' fd
+
+alias h='hx'
+alias s='sudo'
+alias del='rm -rf'
+alias grep='grep --color=auto'
+
+alias xi='sudo xbps-install'
+alias xs='xbps-src-install'
+xbps-src-install() {
+  ~/void-packages/xbps-src pkg $1 && xi --repository hostdir/binpkgs $1
+}
+alias xs-Su='xbps-src-update'
+xbps-src-update() {
+    xi -Su && cd ~/void-packages && git pull && ./xbps-src update-sys
+}
+alias xr='sudo xbps-remove'
+alias xq='sudo xbps-query'
+
+alias g='git'
+alias gi='git init'
+alias ga='git add'
+alias gc='git commit'
+alias gd='git diff'
+alias gs='git status'
+alias gr='git remote'
+alias gl='git log'
+alias gps='git push'
+alias gpl='git pull'
+alias gf='git fetch'
+alias gcl='git clone'
+alias gb='git branch'
+alias gco='git checkout'
+
+alias hxconf='h ~/.config/helix/config.toml'
+alias wezconf='h ~/.config/wezterm/wezterm.lua'
+alias zshconf='h ~/.zshrc'
+alias hyprconf='h ~/.config/hypr/hyprland.conf'
+
+export PROMPT='
+%F{4}%~
+> %f'
+export EDITOR=hx
+export HISTFILE=~/.zsh_history
+export SAVEHIST=16384
+export HISTSIZE=4096
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
